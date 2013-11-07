b766eb85151eb86a84b5803675af7ed685438874
vishwath <vkumar2014@kellogg.northwestern.edu>
2013-11-07 12:27:05 -0600

    Parts 2, 3 of HW complete

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -10,8 +10,19 @@ class PicturesController < ApplicationController
   		@list_of_pictures = Picture.all
   	end
 
-  def new
+  def create
 
+  @p = Picture.new
+  @p.source = params[:source]
+  @p.caption = params[:caption]
+  @p.save
+
+  end
+
+  def destroy
+
+  	@picture_delete = Picture.find(params[:id])
+  	@picture_delete.destroy
   	
 
   end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,10 @@ Rxngif::Application.routes.draw do
 
   get("/create_picture", { :controller => "pictures", :action => "create" })
 
+  get("/delete_picture/:id", { :controller => "pictures", :action => "destroy" })
+
+  get("/edit_picture_form/:id", { :controller => "pictures", :action => "edit" })
+
+  get("/update_picture/:id", { :controller => "pictures", :action => "update" })
 
 end
